77f3866431d947cc8623acf9cf8bb665492df13e
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2023-05-03 16:48:12 +0200

    add dt2days and days2dt

--- src/cadati/np_date.py
+++ src/cadati/np_date.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, TU Wien, Department of Geodesy and Geoinformation
+# Copyright (c) 2023, TU Wien, Department of Geodesy and Geoinformation
 # Distributed under the MIT License (see LICENSE.txt)
 
 """
@@ -9,13 +9,9 @@ import numpy as np
 
 from cadati.check_date import is_leap_year
 
-ref_dt = np.datetime64('1970-01-01')
-ref_jd = 2440587.5  # julian date on 1970-01-01 00:00:00
-
-
 # leap year
-days_past = np.array([0, 31, 60, 91, 121, 152, 182, 213,
-                      244, 274, 305, 335, 366])
+days_past = np.array(
+    [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366])
 
 
 def dt2jd(dt):
@@ -32,7 +28,10 @@ def dt2jd(dt):
     jd : numpy.float64
         Array of julian dates.
     """
-    return (dt - ref_dt)/np.timedelta64(1, 'D') + ref_jd
+    # julian date on 1970-01-01 00:00:00
+    ref_jd = 2440587.5
+
+    return (dt - np.datetime64("1970-01-01")) / np.timedelta64(1, "D") + ref_jd
 
 
 def dt2cal(dt, doy_respect_nonleap_year=True):
@@ -55,7 +54,7 @@ def dt2cal(dt, doy_respect_nonleap_year=True):
         calendar array with last axis representing year, month, day, hour,
         minute, second, millisecond, day of year
     """
-    cal_dt = np.empty(dt.shape + (8,), dtype="u4")
+    cal_dt = np.empty(dt.shape + (8, ), dtype="u4")
 
     Y, M, D, h, m, s = [dt.astype("M8[{}]".format(x)) for x in "YMDhms"]
     cal_dt[..., 0] = Y + 1970
@@ -73,3 +72,46 @@ def dt2cal(dt, doy_respect_nonleap_year=True):
             cal_dt[..., 7] < 60, nonleap_years)
 
     return cal_dt
+
+
+def dt2days(dt, dt_ref=np.datetime64("1900-01-01 00:00:00")):
+    """
+    Convert dates to fraction of days.
+
+    Parameters
+    ----------
+    dt : numpy.datetime64
+        Dates.
+    dt_ref : numpy.datetime64, optional
+        Date reference
+
+    Returns
+    -------
+    days : numpy.ndarray
+        Fraction of days since reference date.
+    """
+    days = (dt - dt_ref) / np.timedelta64(1, "D")
+
+    return days
+
+
+def days2dt(days, dt_ref=np.datetime64("1900-01-01 00:00:00")):
+    """
+    Fraction of days to numpy.datetime64 (rounded to milliseconds.)
+
+    Parameters
+    ----------
+    days : numpy.ndarray
+        Fraction of days since reference date.
+    dt_ref : numpy.datetime64, optional
+        Date reference
+
+    Returns
+    -------
+    dt : numpy.datetime64
+        Dates.
+    """
+    dt = (dt_ref + (days * 24. * 3600 * 1e6).astype("timedelta64[us]") +
+          np.timedelta64(500, "us")).astype("datetime64[ms]")
+
+    return dt
